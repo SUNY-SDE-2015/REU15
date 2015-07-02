e5d39ba216b871ba0ba895691c247c6138771eb7
Kyle M <kylemcgrat@gmail.com>
2015-07-02 14:57:49 -0400

    changes made to coralSimulationwithDelaywithNoise

--- coral/C Code/CoralSimulationwithDelaywithNoise.cpp
+++ coral/C Code/CoralSimulationwithDelaywithNoise.cpp
@@ -69,11 +69,11 @@ double linear(long steps,
 			v[m]=x[2];
 			w[m]=x[3];
 			calcRandom = (calcRandom+1)%2; // update which random number to use.
-			fprintf(fp,"%f,%f,%f,%f,%f,%f\n",x[0],x[1],1-x[0]-x[1],x[2],x[3],1-x[2]-x[3]);
+			//fprintf(fp,"%f,%f,%f,%f,%f,%f\n",x[0],x[1],1-x[0]-x[1],x[2],x[3],1-x[2]-x[3]);
 		}
 
 		//printf("%f\t%f\t%f\t%f\t%f\n",dt,beta,tau,x[0],x[1]);
-		//fprintf(fp,"%f,%f,%f,%i,%f,%f,%f,%f,%f,%f\n",dt,beta,tau,q+1,h,s,1-h-s,x[0],x[1],1-x[0]-x[1]);
+		fprintf(fp,"%f,%f,%f,%i,%f,%f,%f,%f,%f,%f\n",dt,beta,tau,q+1,h,s,1-h-s,x[0],x[1],1-x[0]-x[1]);
 		return 0;
 	}
 
@@ -95,34 +95,34 @@ int main(int argc,char **argv)
 			These are the parameters that are used in the operator for the
 			differential equations.
 		 */
-		double a    	 = 0.1;
-		double g    	 = 0.3;
-		double gamma	 = 0.8;
-		double r    	 = 1.0;
-		double d    	 = 0.44;
-		double tau		 = 0.5;
-		double beta 	 = 0.1;
-		double chi		 = r*gamma/(r+a)-gamma+a;					//Intermediate Step
-		double xi		 = -(d*gamma/(r+a)+a);						//Intermediate Step	
-		double cbar		 = (-xi-sqrt(xi*xi-4*chi*g))/(2*chi);		//Intermediate Step
-		double zeta		 = 1-cbar;									//Saddle point value for coral
-		double eta		 = (r-r*zeta-d)/(r+a);						//Saddle point value for macroalgae
-		double gZero	 = ((d*a*r+d*d)*(gamma-a))/(r*r);
-		double gOne		 = (gamma*(a+d))/(a+r);
-		double omega	 = sqrt((r*r*(g*g-gNil*gNil))/(d*d));
-		double tauZero	 = (1/omega)*acos(gNil/g);		
+		double a    	 	= 0.1;
+		double gamma	 	= 0.8;
+		double r    	 	= 1.0;
+		double d    	 	= 0.44;
+		double beta 	 	= 0;
+		double chi		= r*gamma/(r+a)-gamma+a;					//Intermediate Step
+		double xi		= -(d*gamma/(r+a)+a);						//Intermediate Step	
+		double gZero	 	= ((d*a*r+d*d)*(gamma-a))/(r*r);
+		double gOne		= (gamma*(a+d))/(a+r);
+		double g    	 	= 0.5;
+		double cbar		= (-xi-sqrt(xi*xi-4*chi*g))/(2*chi);		//Intermediate Step
+		double omega	 	= sqrt((r*r*(g*g-gZero*gZero))/(d*d));
+		double tauZero	 	= (1/omega)*acos(gZero/g);		
+		double coralSaddle	= 1-cbar;									//Saddle point value for coral
+		double macroSaddle	= (r-r*coralSaddle-d)/(r+a);						//Saddle point value for macroalgae
 
 		double dt,final;    // The time step and the final time.
 		int trials;         // The number of simulations to make.
 		
-		final=25;  // Set the final time.
+		final=100;  // Set the final time.
 		trials=1; // Set the number of trials to perform.
 		
 		// Set the smallest time step
 		dt=0.0001;
 		
 		// Set tau
-		double tau		 = 2*tauZero;
+		double tau		 = .5;
+		printf("tau is %f", tau);
 		
 		// Sets the seed for the random numbers
 		srand48(time(NULL));
@@ -140,9 +140,10 @@ int main(int argc,char **argv)
 		
 		// Create a CSV File
 		FILE*fp;
+                //String fileName = "trials-g" + std::toString(g) + "-tau" + std::toString(tau);
 		fp=fopen("trials.csv","w");
-		//fprintf(fp,"dt,beta,tau,trial,initMacro,initCoral,initTurf,macroalgae,coral,turf\n");
-		fprintf(fp,"macroalgae,coral,turf,lgmacroalgae,lgcoral,lgturf\n");
+		fprintf(fp,"dt,beta,tau,trial,initMacro,initCoral,initTurf,macroalgae,coral,turf\n");
+		//fprintf(fp,"macroalgae,coral,turf,lgmacroalgae,lgcoral,lgturf\n");
 		//printf("dt\t\tbeta\t\ttau\t\tMacroalgae\tCoral\n");
 		
 		
@@ -161,10 +162,14 @@ int main(int argc,char **argv)
 						steps=(long)(final/dt);
 						for (int k=0;k<trials;k++)
 						{
-							y[0]=zeta; //initial Macroalgae level
-							z[0]=eta; //initial Coral level
-							v[0]=zeta;
-							w[0]=eta;
+							//y[0]=coralSaddle; //initial Macroalgae level
+							//z[0]=macroSaddle; //initial Coral level
+							//v[0]=coralSaddle;
+							//w[0]=macroSaddle;
+							y[0]=.33; //initial Macroalgae level
+							z[0]=.44; //initial Coral level
+							v[0]=.33;
+							w[0]=.44;
 							for (int l=1;l<n;l++) //fills in "negative" times for both y and z
 							{
 								y[l]=y[0];
@@ -172,8 +177,8 @@ int main(int argc,char **argv)
 								v[l]=v[0];
 								w[l]=w[0];
 							}
-							fprintf(fp,"%f,%f,%f,%f,%f,%f\n",y[0],z[0],1-y[0]-z[0],v[0],w[0],1-v[0]-w[0]);
-							linear(steps,a,gamma,r,d,g,x,y,z,dt,n,beta,tau,fp,k,h,s,v,w);
+							//fprintf(fp,"%f,%f,%f,%f,%f,%f\n",y[0],z[0],1-y[0]-z[0],v[0],w[0],1-v[0]-w[0]);
+							linear(steps,a,gamma,r,d,g,x,y,z,dt,n,beta,tau,fp,k,coralSaddle,macroSaddle,v,w);
 						}
 					}
 					dt=dt+0.0001;
